aa650c32d15288ef60b0a4b55bfc5cbcdc7a693a
Bert Verhelst <verhelstbert@gmail.com>
2026-06-30 19:54:42 +0200

    fix: fix issue with installing the browser extension

---
+++ chrome-extension/src/options/options.js
@@ -0,0 +1,14 @@
+const apiUrlInput = document.getElementById('api-url');
+const saveButton = document.getElementById('save');
+const statusDiv = document.getElementById('status');
+
+chrome.storage.sync.get(['apiUrl'], (result) => {
+  apiUrlInput.value = result.apiUrl || 'http://localhost:55577/api';
+});
+
+saveButton.addEventListener('click', () => {
+  chrome.storage.sync.set({ apiUrl: apiUrlInput.value }, () => {
+    statusDiv.textContent = 'Options saved.';
+    setTimeout(() => { statusDiv.textContent = ''; }, 2000);
+  });
+});
